6e2968e2cfaf8277cd90b5a43a5b4cedac653146
gtraintradition <gtraintradition@gmail.com>
2024-12-22 17:49:23 +0100

    added player drawing code

--- main.py
+++ main.py
@@ -4,17 +4,28 @@
 import pygame
 
 from constants import *
-
+from player import Player
 
 
 def main():
     pygame.init()
 
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    dt = 0
+
+    # player spawn coordinates
+    spawn_x = SCREEN_WIDTH / 2
+    spawn_y = SCREEN_HEIGHT / 2
+    
+    player_1 = Player(spawn_x, spawn_y)
+
+
     print(f"Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
 
     while True:
 
@@ -23,7 +34,12 @@ def main():
                 return
             
         screen.fill((0, 0, 0))
+        player_1.draw(screen)
         pygame.display.flip()
+
+        # framerate limit at: 60 FPS
+        dt = game_clock.tick(60) / 1000
+
 
 
 
