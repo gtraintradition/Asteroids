5a3e83775ea8faa32dec210a0bef7a7bc6ced280
gtraintradition <gtraintradition@gmail.com>
2024-12-22 04:36:35 +0100

    main.py created

---
+++ main.py
@@ -0,0 +1,12 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+
+
+def main():
+    print(f"Starting asteroids!")
+
+
+if __name__ == "__main__":
+    main()
